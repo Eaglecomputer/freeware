9d878276a165cf92b08bb2837fe7002f292ada3e
Eaglecomputer <ecom.hunter@gmail.com>
2017-12-06 01:44:14 +0900

    Add files via upload
    Powershell版Paste Sample for Shift_JIS

---
+++ Paste.ps1
@@ -0,0 +1,8 @@
+#Powershell”ÅPast Sample for Shift_JIS
+$1=gc a.txt
+$2=gc b.txt
+for($count=0;$count -lt $1.Length;$count++){
+	$1c=$1[$count] -replace "`r`n",""
+	$2c=$2[$count] -replace "`r`n",""
+	echo $1c","$2c" |Out-File Past.txt -Encoding Default -Append
+}
