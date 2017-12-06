a3ee03b82ab43f6c9085f0cb021851fb393373a0
Eaglecomputer <ecom.hunter@gmail.com>
2017-12-07 00:51:12 +0900

    Delete Paste.ps1

--- Paste.ps1
+++
@@ -1,8 +0,0 @@
-#Powershell”ÅPaste Sample for Shift_JIS
-$1=gc a.txt
-$2=gc b.txt
-for($count=0;$count -lt $1.Length;$count++){
-	$1c=$1[$count] -replace "`r`n",""
-	$2c=$2[$count] -replace "`r`n",""
-	echo $1c","$2c |Out-File Past.txt -Encoding Default -Append
-}
