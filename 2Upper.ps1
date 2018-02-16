8e61cb63f2f7fcb5a275b398b7222104a5672202
Eaglecomputer <ecom.hunter@gmail.com>
2018-02-16 18:03:02 +0000

    Delete 2Upper.ps1

--- 2Upper.ps1
+++
@@ -1,7 +0,0 @@
-#¬•¶š‚ğ‘å•¶š‚É•ÏŠ·
-param($a)
-Add-Type -AssemblyName Microsoft.VisualBasic
-$src=(Get-Content $a -Encoding "String") -Join "`r`n"
-
-$work=[Microsoft.VisualBasic.Strings]::StrConv($src,[Microsoft.VisualBasic.VbStrConv]::Uppercase)
-$work
