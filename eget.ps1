77c007ef05abb9029169b6961003c713ee44ebd6
Eaglecomputer <ecom.hunter@gmail.com>
2018-02-16 18:15:55 +0000

    Delete eget.ps1

--- eget.ps1
+++
@@ -1,6 +0,0 @@
-Param($a)
-
-$b=New-Object System.Net.WebClient
-$file=[System.IO.Path]::GetFileName($a)
-#$b.DownloadFile($a,(Join-Path "R:\" $file))
-$b.DownloadFile($a,(Join-Path (Convert-Path .)$file))
