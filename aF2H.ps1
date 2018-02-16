d1f8ee723e28abcaad5e9cb2c772be0b8b0ab6df
Eaglecomputer <ecom.hunter@gmail.com>
2018-02-16 18:10:43 +0000

    Delete aF2H.ps1

--- aF2H.ps1
+++
@@ -1,7 +0,0 @@
-#‘SŠp‚ð”¼Šp‚É•ÏŠ·
-param($a)
-Add-Type -AssemblyName Microsoft.VisualBasic
-$src=Get-Content $a -Encoding "String"
-
-$narrow=[Microsoft.VisualBasic.Strings]::StrConv($src,[Microsoft.VisualBasic.VbStrConv]::Narrow)
-$narrow
