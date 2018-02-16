f9c19c40f69cd39510fc1b5276f6601b41c0636f
Eaglecomputer <ecom.hunter@gmail.com>
2018-02-16 18:18:50 +0000

    Delete md5.ps1

--- md5.ps1
+++
@@ -1,8 +0,0 @@
-#MD5ÇÃê∂ê¨
-param($a)
-$file=New-Object IO.StreamReader $a
-$md5=[System.Security.Cryptography.MD5]::Create()
-$hash=$md5.ComputeHash($file.BaseStream);
-$result=[System.BitConverter]::ToString($hash).ToLower().Replace("-","")+" *"+$a
-
-Write-Output $result
