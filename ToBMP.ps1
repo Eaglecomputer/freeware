f58be10ff6d0d44f20f35569b3fc1302d71088e9
Eaglecomputer <ecom.hunter@gmail.com>
2018-02-16 18:09:20 +0000

    Delete ToBMP.ps1

--- ToBMP.ps1
+++
@@ -1,9 +0,0 @@
-#ï«éÜÇÃê∂ê¨
-Param($a)
-Add-Type -AssemblyName System.Drawing
-
-$Image=New-Object System.Drawing.Bitmap -ArgumentList $a
-$canvas=[System.Drawing.Graphics]::FromImage($Image)
-
-$Image.Save((Get-ChildItem $a).BaseName+".bmp",[System.Drawing.Imaging.ImageFormat]::Bmp)
-$Image.Dispose()
